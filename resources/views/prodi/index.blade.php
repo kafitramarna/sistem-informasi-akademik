9eed85565b8d30b5b68652df863f3a5894cb1b07
kafitramarna <kafitra.marna@gmail.com>
2024-05-05 11:21:56 +0700

    tambah prodi

---
+++ resources/views/prodi/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title')
+    Prodi
+@endSection
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <div class="w-100 text-end">
+                <a href="" class="btn btn-primary">Tambah Prodi</a>
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table table-responsive">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Prodi</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>       
+@endSection
